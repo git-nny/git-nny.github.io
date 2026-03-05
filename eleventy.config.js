6f7c326bf83245265952f7d384cf5bb27b98b83e
git-nny <147989339+git-nny@users.noreply.github.com>
2026-03-05 22:04:43 +0100

    New Test

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_src/_style")
   eleventyConfig.addPassthroughCopy("_src/_scripts")
   eleventyConfig.addPassthroughCopy("_src/_assets/")
+  eleventyConfig.addPassthroughCopy("CNAME")
 
   return {
     markdownTemplateEngine: "njk",
