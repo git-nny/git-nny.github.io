6ef8c75ba784358ea884b3bcf37ae5e57183b158
git-nny <147989339+git-nny@users.noreply.github.com>
2026-03-21 12:38:49 +0100

    Fixed Data Preprocessing with and controversy

--- _src/projects/26_eurovision/26_eurovision.js
+++ _src/projects/26_eurovision/26_eurovision.js
@@ -1,5 +1,6 @@
 import * as d3 from "d3"; // TODO: Only import what we actually need at the end
-import { loadCSV, op } from 'arquero';
+import * as aq from 'arquero'; // TODO: Only import what we actually need at the end
+import {op} from 'arquero'
 
 // Good variables
 const FILE = "./song_data.csv"
@@ -12,7 +13,7 @@ let xAxisTranslate = HEIGTH - 80;
 let xAxisTranslateO = HEIGTH - 110;
 
 // magic numbers TODO fix!
-const MaxX = 850; //
+const MaxX = 850; //??
 const MaxY = 450;
 let AllCountries = ['','Albania', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium','Bulgaria', 'Croatia',
  'Cyprus', 'Czechia', 'Denmark', 'Estonia', 'Finland', 'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Israel', 'Italy',
@@ -24,11 +25,15 @@ const jitterWidth = 3;
 const jitterWidth2 = -3;
 
 //load data as table and get relevant participants since 2016
-let data = await loadCSV(FILE)
+let data = await aq.loadCSV(FILE)
 data = data.filter( d => d.year >= 2016 ).filter( d => d.final_jury_points !== null ).reify()
-// console.log(data)
 
+data = data
+  .derive({ controversy: d => op.abs(d.final_televote_points - d.final_jury_points)})
+  .orderby( 'controversy')
+  .derive({rank: d => `${op.row_number()}` })
 
+console.log(data.column('rank'))
 // getting an array of numbers for the scale Point Scale
 for (let i = 0; i<=NumTeilnehmer; i++){
     NumTeilnehmerArray.push(`${i}`);
@@ -102,42 +107,77 @@ SVG.append("text")
         .attr('x', -HEIGTH/2)
         .text('Points per mode');
 
+function drawInitialPlot(){
+  SVG.append('g')
+  .attr('id', 'marks')
+  .selectAll()
+  .data(data)
+  .enter()
+  .append('line')
+    .attr('class', 'connect')
+    .attr('x1', function (d){return xScale(d.final_total_points);})
+    .attr ('y1', function (d){return yScale(d.final_jury_points);})
+    .attr('x2', function (d){return xScale(d.final_total_points);})
+    .attr ('y2', function (d){return yScale(d.final_televote_points);})
+    .style('stroke-width', 1.5);
+  
 
+d3.select('#marks')
+  .selectAll()
+  .data(data)
+  .enter()
+  .append('circle')
+    .attr('class', 'televote')
+    .attr("cx", function (d) { return xScale(d.final_total_points); } )
+    .attr("cy", function (d) { return yScale(d.final_televote_points); } )
+    .attr("r", 4)
 
+d3.select('#marks')
+  .selectAll()
+  .data(data)
+  .enter()
+  .append('circle')
+    .attr('class', 'jury')
+    .attr("cx", function (d) { return xScale(d.final_total_points); } )
+    .attr("cy", function (d) { return yScale(d.final_jury_points); } )
+    .attr("r", 4)
+}
 
+drawInitialPlot()
 
 function plotTotalPoints (){
   xScale.domain([0, MaxX])
-        SVG.selectAll('.xaxis').remove();
-        SVG.append('g').attr("class","xaxis")
-            .attr('transform', 'translate(55,'+ xAxisTranslate + ')')
-            .call(x_axis);
+  SVG.selectAll('.xaxis').remove();
+  SVG.append('g').attr("class","xaxis")
+    .attr('transform', 'translate(55,'+ xAxisTranslate + ')')
+    .call(x_axis);
 
-        SVG.selectAll('#yaxis').remove();
-        SVG.append('g').attr('id', 'yaxis')
-        .attr('transform', 'translate(55,60)').call(y_axis);
-        SVG.selectAll('.connect').transition().attr('visibility', 'visible');
+  SVG.selectAll('#yaxis').remove();
+  SVG.append('g').attr('id', 'yaxis')
+  .attr('transform', 'translate(55,60)').call(y_axis);
+  SVG.selectAll('.connect').transition().attr('visibility', 'visible');
 
         
-        SVG.selectAll('.jury-circle').transition()
-        .attr("cy", function (d) { return yScale(d.final_jury_points); } ) 
-        .attr('cx', function(d) {return xScale(d.final_total_points)})
-        .attr('transform', 'translate(55,60)');
-        SVG.selectAll('.televote-circle').transition()
-        .attr("cy", function (d) { return yScale(d.final_televote_points); } ) 
-        .attr('cx', function(d) {return xScale(d.final_total_points)})
-        .attr('transform', 'translate(55,60)');
-        SVG.selectAll('.connect').transition()
-        .attr('opacity', '1')
-        .attr('x1', function (d) { return xScale( d.final_total_points)} )
-        .attr('y1', function (d) {return yScale (d.final_jury_points)})
-        .attr('x2', function (d) {return xScale( d.final_total_points)} )
-        .attr('y2', (d) => yScale(d.final_televote_points))
-        .attr('transform', 'translate(55,60)');
+  SVG.selectAll('.jury').transition()
+    .attr("cy", function (d) { return yScale(d.final_jury_points); } ) 
+    .attr('cx', function(d) {return xScale(d.final_total_points)})
+    .attr('transform', 'translate(55,60)');
+  
+  SVG.selectAll('.televote').transition()
+    .attr("cy", function (d) { return yScale(d.final_televote_points); } ) 
+    .attr('cx', function(d) {return xScale(d.final_total_points)})
+    .attr('transform', 'translate(55,60)');
+  SVG.selectAll('.connect').transition()
+    .attr('opacity', '1')
+    .attr('x1', function (d) { return xScale( d.final_total_points)} )
+    .attr('y1', function (d) {return yScale (d.final_jury_points)})
+    .attr('x2', function (d) {return xScale( d.final_total_points)} )
+    .attr('y2', (d) => yScale(d.final_televote_points))
+    .attr('transform', 'translate(55,60)');
 
 
-        SVG.select('.xlabel').transition().text('Total Points');
-        SVG.select('.ylabel').transition().text('Points per mode');
+  SVG.select('.xlabel').transition().text('Total Points');
+  SVG.select('.ylabel').transition().text('Points per mode');
 }
 function plotDifference () {
   SVG.selectAll('.xaxis').remove();
@@ -154,22 +194,22 @@ function plotDifference () {
         SVG.select('.xlabel').text('Controversy');
         SVG.select('.ylabel').text('Difference');
         
-        SVG.selectAll('.televote-circle')
+        SVG.selectAll('.televote')
             .attr('cy', yScaleS('0'))
             .attr('cx', function(d) {return xScaleS(d.rank)})
             .attr('transform', 'translate(55,60)');
         
 
-        SVG.selectAll('.jury-circle').transition()
+        SVG.selectAll('.jury').transition()
             .attr('cy', function (d) { return yScaleS( Math.abs(d.final_televote_points-d.final_jury_points))}) 
             .attr('cx', function(d) {return xScaleS ( d.rank)})
             .attr('transform', 'translate(55,60)');
         SVG.selectAll('.connect').transition()
         .attr('opacity', '1')
-        .attr('x1', function (d) { return xScaleS(d.rank);} )
+        .attr('x1', function (d) { return xScaleS(d.rank);} ) // TODO Rang Problem
         .attr('y1', yScale('0'))
         .attr('x2', function (d) {return xScaleS( d.rank);} )
-        .attr('y2', function(d) {return yScaleS( Math.abs(d.final_televote_points-d.final_jury_points))})
+        .attr('y2', function(d) {return yScaleS( d.controversy)})
         .attr('transform', 'translate(55,60)');
 }
 function plotCountry (){
@@ -187,11 +227,11 @@ function plotCountry (){
         
         SVG.selectAll('.connect').transition().attr('visibility', 'hidden');
 
-        SVG.selectAll('.jury-circle').transition()
+        SVG.selectAll('.jury').transition()
             .attr('cx', function(d){return xScaleO (d.country)+ xScaleO.bandwidth()/2 - Math.random()*jitterWidth})
             .attr('cy', function(d){return yScale (d.final_jury_points)})
             .attr('transform', 'translate(55,29)');
-        SVG.selectAll('.televote-circle').transition()
+        SVG.selectAll('.televote').transition()
             .attr('cx', function(d){return xScaleO (d.country)+ xScaleO.bandwidth()/2 - Math.random()*jitterWidth2})
             .attr('cy', function(d){return yScale (d.final_televote_points)})
             .attr('transform', 'translate(55,29)');
@@ -204,49 +244,79 @@ function plotCountry (){
 
 plotTotalPoints()
 
-// better: get checked checkbox and work with that?
+// Buttons + Functions
 function changePlot (id){
   if (id === 'rplottot'  ){
     plotTotalPoints()
   } else if  (id === 'rplotdif') {
     plotDifference()
-  } else {
+  } else if (id == 'rplotcountry') {
     plotCountry()
   }
+  
 }
-document.querySelector('#rplottot').addEventListener('onclick', changePlot('rplottot'))
 
-
-
-function startToolTip(year, country, artist_name, song_name){
-     d3.select("#tooltip").style('display', 'block');
-     d3.select("#country").html(country + ' ' + year );
-     d3.select('#artist').html(artist_name);
-     d3.select('#song').html(song_name); 
-     d3.selectAll('circle').transition().attr('r', '3').style('opacity', '.1');
-     d3.selectAll('.connect').transition().style('opacity', '.05');
-     d3.selectAll('.y'+ year).transition().attr('r', '6').style('opacity', '1');
-     d3.select('#EV-flag-spez').attr('src', 'flags/1x1/' + country + '.SVG')
-    
+let hideYear = function(name){
+  d3.selectAll('.y'+name)
+    .style('display', 'none');
 }
+
+let showYear = function(name){
+  d3.selectAll('.y'+name)
+  .style('display', 'block');
+}
+
+
+function toggleCheckbox (name, id) {
+    console.log(id)
+  let checkBox = document.getElementById(id);
+  if (checkBox.checked === true){
+    showYear(name);
+  } else {
+    hideYear(name);
+  }
+} 
+
+// add Button functionalities
+for (let radio of document.querySelectorAll("input[type=radio]")) {
+  radio.addEventListener('click', () => {changePlot(radio.id)})
+}
+for (let checkbox of document.querySelectorAll("input[type=checkbox]")) {
+  checkbox.addEventListener('change', () => { toggleCheckbox(checkbox.name, checkbox.id) })
+}
+
+
+
+
+// function startToolTip(year, country, artist_name, song_name){
+//      d3.select("#tooltip").style('display', 'block');
+//      d3.select("#country").html(country + ' ' + year );
+//      d3.select('#artist').html(artist_name);
+//      d3.select('#song').html(song_name); 
+//      d3.selectAll('circle').transition().attr('r', '3').style('opacity', '.1');
+//      d3.selectAll('.connect').transition().style('opacity', '.05');
+//      d3.selectAll('.y'+ year).transition().attr('r', '6').style('opacity', '1');
+//     //  d3.select('#EV-flag-spez').attr('src', 'flags/1x1/' + country + '.SVG')
+    
+// }
     
     
-function stopToolTip (year){
-    d3.select("#tooltip").style('display', 'none');
-    d3.selectAll('circle').transition().attr('r', '4').style('opacity','1');
-    d3.selectAll('.connect').transition().attr('r', '4').style('opacity','1');
+// function stopToolTip (year){
+//     d3.select("#tooltip").style('display', 'none');
+//     d3.selectAll('circle').transition().attr('r', '4').style('opacity','1');
+//     d3.selectAll('.connect').transition().attr('r', '4').style('opacity','1');
         
  
-     } 
+//      } 
         
-stopToolTip(); 
+// stopToolTip(); 
   
-    let tooltip = d3.select('#datavis')
-    .on('mousemove', function(event){
-        let coords = d3.pointer(event);
-        d3.select("#tooltip").style('top', 50+coords[1]+"px")
-                             .style('left', 45+ coords[0]+"px");
-            })
+//     let tooltip = d3.select('#datavis')
+//     .on('mousemove', function(event){
+//         let coords = d3.pointer(event);
+//         d3.select("#tooltip").style('top', 50+coords[1]+"px")
+//                              .style('left', 45+ coords[0]+"px");
+//             })
 
   
 
@@ -276,66 +346,9 @@ stopToolTip();
 //     .call(y_axis);
 
 // // 
-// function drawClevelandPlot(){
-//   SVG.append('g')
-//   .attr('id', 'marks')
-//   .selectAll()
-//   .data(data)
-//   .enter()
-//   .append('line')
-//   .attr('x1', function (d){return xScale(d.final_total_points);})
-//   .attr ('y1', function (d){return yScale(d.final_jury_points);})
-//   .attr('x2', function (d){return xScale(d.final_total_points);})
-//   .attr ('y2', function (d){return yScale(d.final_televote_points);})
-//   .style('stroke-width', 1.5);
-  
 
-// d3.select('#marks')
-//   .selectAll()
-//   .data(data)
-//   .enter()
-//   .append('circle')
-//     .attr('class', 'televote')
-//     .attr("cx", function (d) { return xScale(d.final_total_points); } )
-//     .attr("cy", function (d) { return yScale(d.final_televote_points); } )
-//     .attr("r", 4)
-
-// d3.select('#marks')
-//   .selectAll()
-//   .data(data)
-//   .enter()
-//   .append('circle')
-//     .attr('class', 'jury')
-//     .attr("cx", function (d) { return xScale(d.final_total_points); } )
-//     .attr("cy", function (d) { return yScale(d.final_jury_points); } )
-//     .attr("r", 4)
-// }
 // function controversy (){
 //   console.log(data.columnNames())
 //   //get max value of column
   
 // }
-
-
-// drawClevelandPlot()
-// controversy()
-
-
-
-
-
-// Vis 2: controversy + check separate quartiles
-// Vis 3: Check Country/Genre/prequalified vs. qualifier
-
-
-
-
-
-/* So--- what changed?
-full data set!
-using arquero (I miss u, pandas) for data manipulation instead of some of the stuff I did in 2023 with d3 and a dream
-this could be object oriented? maybe?
-collections of sins:
-  - actually, <300 lines is not too bad - still, a lot of redundancy
-  - hardcoded all country names
-*/
