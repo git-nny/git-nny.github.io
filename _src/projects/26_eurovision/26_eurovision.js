a73a8087bc44191bbd85ff7a8781679f4c52999f
git-nny <147989339+git-nny@users.noreply.github.com>
2026-03-23 13:26:28 +0100

    Removed unnecessary imports, let's go!

--- _src/projects/26_eurovision/26_eurovision.js
+++ _src/projects/26_eurovision/26_eurovision.js
@@ -1,6 +1,6 @@
-import * as d3 from "d3"; // TODO: Only import what we actually need at the end
-import * as aq from 'arquero'; // TODO: Only import what we actually need at the end
-import {op} from 'arquero'
+
+import { select, selectAll, scaleLinear, scalePoint, axisBottom, axisLeft, easeBack, pointer } from "d3";
+import {op, loadCSV} from 'arquero'
 
 // Good variables
 const FILE = "./song_data.csv"
@@ -8,7 +8,7 @@ let REM = window.getComputedStyle(document.querySelector('body')).fontSize.slice
 let MARGIN = 2* REM
 let HEIGTH = window.innerHeight - document.querySelector('#head').clientHeight - MARGIN
 let WIDTH = window.innerWidth - 2*MARGIN
-let SVG = d3.select('#datavis').attr('height', HEIGTH).attr('width', WIDTH)
+let SVG = select('#datavis').attr('height', HEIGTH).attr('width', WIDTH)
 let VISWIDTH = [0, WIDTH - 4*MARGIN]
 let VISHEIGHT = [0, HEIGTH - 2.5*MARGIN ]
 
@@ -30,7 +30,7 @@ const jitterWidth2 = -3;
 let MedianIsVisible = true
 
 //load data as table and get relevant participants since 2016
-let data = await aq.loadCSV(FILE)
+let data = await loadCSV(FILE)
 data = data.filter( d => d.year >= 2016 ).filter( d => d.final_total_points !== null ).reify()
 
 data = data
@@ -50,49 +50,49 @@ for (let i = 0; i<=NumTeilnehmer; i++){
 }
 
 // setting up X and Y scales for linear and point modes
-let yScale = d3.scaleLinear()
+let yScale = scaleLinear()
     .domain([MaxY,0])
     .range(VISHEIGHT);
-let yScaleS = d3.scaleLinear() 
+let yScaleS = scaleLinear() 
     .domain([260,0])
     .range(VISHEIGHT);
 
-let xScale = d3.scaleLinear()
+let xScale = scaleLinear()
     .domain([0, MaxX]) 
     .range(VISWIDTH);
-let yScaleO = d3.scaleLinear()
+let yScaleO = scaleLinear()
     .domain([MaxY,0])
     .range([0, HEIGTH - 5*REM ]);
 
-let xScaleO = d3.scalePoint() 
+let xScaleO = scalePoint() 
     .domain(AllCountries)
     .range(VISWIDTH); 
 
-let xScaleS = d3.scalePoint() 
+let xScaleS = scalePoint() 
     .domain (NumTeilnehmerArray)
     .range(VISWIDTH); 
 
 
 //setting up X and Y axis for linear and point modes
-  let x_axis = d3.axisBottom()
+  let x_axis = axisBottom()
       .scale(xScale)
       .tickSize((VISHEIGHT[1]));
-  let x_axisS = d3.axisBottom()
+  let x_axisS = axisBottom()
       .scale(xScaleS)
       .tickSize((VISHEIGHT[1]));
-  let x_axisO = d3.axisBottom()
+  let x_axisO = axisBottom()
       .scale(xScaleO)
       .tickSize(- (VISHEIGHT[1]));
 
   //Axis Y
-  let y_axis = d3.axisLeft()
+  let y_axis = axisLeft()
       .scale(yScale)
       .tickSize(-(VISWIDTH[1]));
       
-  let y_axisO = d3.axisLeft()
+  let y_axisO = axisLeft()
       .scale(yScaleO)
       .tickSize(-(VISWIDTH[1]));
-  let y_axisS = d3.axisLeft()
+  let y_axisS = axisLeft()
       .scale(yScaleS)
       .tickSize(-(VISWIDTH[1]));
 
@@ -152,11 +152,11 @@ SVG.append("text")
 
         
 function resetModus(){
-  d3.selectAll('.connect')
+  selectAll('.connect')
     .attr('stroke-width', '2')
     .attr('visibility', 'visible')
 
-  d3.selectAll(".jury, .televote")
+  selectAll(".jury, .televote")
     .attr('opacity', 1)
     .attr('visibility', 'visible')
 
@@ -179,7 +179,7 @@ function drawInitialPlot(){
     .attr ('y2', function (d){return yScale(d.final_televote_points);})
     .attr('stroke-width', '2')
 
-d3.select('#marks')
+select('#marks')
   .selectAll()
   .data(data)
   .enter()
@@ -190,7 +190,7 @@ d3.select('#marks')
     .on('mouseover', (event, d) => startToolTip(d.year,d.country,d.artist_name, d.song_name))
     .on('mouseout', (event, d) => stopToolTip(d.country))
 
-d3.select('#marks')
+select('#marks')
   .selectAll()
   .data(data)
   .enter()
@@ -201,24 +201,24 @@ d3.select('#marks')
     .on('mouseover', (event, d) => startToolTip(d.year,d.country,d.artist_name, d.song_name))
     .on('mouseout', (event, d) => stopToolTip(d.country))
 
-  d3.select('#marks').attr('transform', `translate(${xAxisTranslate}, ${yAxisTranslate})`)
+  select('#marks').attr('transform', `translate(${xAxisTranslate}, ${yAxisTranslate})`)
 }
 function plotTotalPoints (){
   drawLinearAxis()
   resetModus()
     
   SVG.selectAll('.jury').transition()
-    .duration(800).ease(d3.easeBack)
+    .duration(800).ease(easeBack)
     .attr("cy", function (d) { return yScale(d.final_jury_points) } ) 
     .attr('cx', function(d) {return xScale(d.final_total_points)})
   
   SVG.selectAll('.televote').transition()
-    .duration(800).ease(d3.easeBack)
+    .duration(800).ease(easeBack)
     .attr("cy", function (d) { return yScale(d.final_televote_points); } ) 
     .attr('cx', function(d) {return xScale(d.final_total_points)})
 
   SVG.selectAll('.connect').transition()
-    .duration(800).ease(d3.easeBack)
+    .duration(800).ease(easeBack)
     .attr('visibility', 'visible')
     .attr('opacity', '1')
     .attr('x1', function (d) { return xScale( d.final_total_points)} )
@@ -227,7 +227,7 @@ function plotTotalPoints (){
     .attr('y2', (d) => yScale(d.final_televote_points))
     
 
-  d3.select('#marks').attr('transform', `translate(${xAxisTranslate}, ${yAxisTranslate})`)
+  select('#marks').attr('transform', `translate(${xAxisTranslate}, ${yAxisTranslate})`)
 
   SVG.select('.xlabel').transition().text('Total Points');
   SVG.select('.ylabel').transition().text('Points per mode');
@@ -237,19 +237,19 @@ function plotDifference () {
   drawControversyAxis()
 
   SVG.selectAll('.televote')
-    .transition().duration(800).ease(d3.easeBack)
+    .transition().duration(800).ease(easeBack)
     .attr('cy', yScaleS('0'))
     .attr('cx', function(d) {return xScaleS(d.rank)})
     .attr('visibility', 'hidden')
 
   SVG.selectAll('.jury')
-    .transition().duration(800).ease(d3.easeBack)
+    .transition().duration(800).ease(easeBack)
     .attr('cy', function (d) { return yScaleS( Math.abs(d.final_televote_points-d.final_jury_points))}) 
     .attr('cx', function(d) {return xScaleS ( d.rank)})
     .attr('visibility', 'hidden')
 
   SVG.selectAll('.connect')
-    .transition().duration(800).ease(d3.easeBack)
+    .transition().duration(800).ease(easeBack)
     .attr('visibility', 'visible')
     .attr('opacity', '1')
       .attr('x1', function (d) { return xScaleS(d.rank);} )
@@ -262,7 +262,7 @@ function plotDifference () {
       .on('mouseover', (event, d) => startToolTip(d.year,d.country,d.artist_name, d.song_name))
       .on('mouseout', (event, d) => stopToolTip(d.country))
 
-  d3.select('#marks').attr('transform', `translate(${xAxisTranslate}, ${yAxisTranslate})`)
+  select('#marks').attr('transform', `translate(${xAxisTranslate}, ${yAxisTranslate})`)
 
   SVG.selectAll('#xaxis .tick text').attr('opacity', '0');
   SVG.select('.xlabel').text('Controversy');
@@ -277,19 +277,19 @@ function plotCountry (){
   showMedianPoints()
             
   SVG.selectAll('.connect').transition()
-    .duration(800).ease(d3.easeBack)
+    .duration(800).ease(easeBack)
     .attr('opacity', '0').attr('visibility', 'hidden');
 
   SVG.selectAll('.jury')
-    .transition().duration(800).ease(d3.easeBack)
+    .transition().duration(800).ease(easeBack)
     .attr('cx', function(d){return xScaleO (d.country)+ xScaleO.bandwidth()/2 - Math.random()*jitterWidth})
     .attr('cy', function(d){return yScale (d.final_jury_points)})
 
-  SVG.selectAll('.televote').transition().duration(800).ease(d3.easeBack)
+  SVG.selectAll('.televote').transition().duration(800).ease(easeBack)
     .attr('cx', function(d){return xScaleO (d.country)+ xScaleO.bandwidth()/2 - Math.random()*jitterWidth2})
     .attr('cy', function(d){return yScale (d.final_televote_points)})
 
-  d3.select('#marks').attr('transform', `translate(${xAxisTranslate}, ${-2*REM})`)
+  select('#marks').attr('transform', `translate(${xAxisTranslate}, ${-2*REM})`)
 
   SVG.select('.xlabel').transition().text('Country');
   SVG.select('.ylabel').transition().text('Points per mode');
@@ -304,7 +304,7 @@ document.getElementById('toggleMedian').addEventListener('click', () => {
 })
 function showMedianPoints (){
   if (MedianIsVisible) {
-    d3.select('#marks')
+    select('#marks')
     .selectAll()
     .data(data)
     .enter()
@@ -318,10 +318,10 @@ function showMedianPoints (){
   
 }
 function exitMedian(){
-  d3.selectAll('.median')
+  selectAll('.median')
     .transition()
     .duration(200)
-    .ease(d3.easeBack)
+    .ease(easeBack)
     .attr('opacity', '0')
     .attr('display', 'none')
 }
@@ -338,32 +338,15 @@ function changePlot (id){
   }
   
 }
-// change on window resize
-// function reloadVis(){
-//   // reload variables
 
-//   REM = window.getComputedStyle(document.querySelector('body')).fontSize.slice(0,2)
-//   MARGIN = 2* REM
-//   HEIGTH = window.innerHeight - document.querySelector('#head').clientHeight - MARGIN
-//   WIDTH = window.innerWidth - 2*MARGIN
-//   SVG = d3.select('#datavis').attr('height', HEIGTH).attr('width', WIDTH)
-//   VISWIDTH = [0, WIDTH - 4*MARGIN]
-//   VISHEIGHT = [0, HEIGTH - 2.5*MARGIN ]
-
-//   const activeID = document.querySelectorAll("input[type=radio]:checked")
-//   changePlot(activeID)
-//   console.log('test')
-// }
-
-// window.onresize = reloadVis
 
 let hideYear = function(name){
-  d3.selectAll('.y'+name)
+  selectAll('.y'+name)
     .style('display', 'none');
 }
 
 let showYear = function(name){
-  d3.selectAll('.y'+name)
+  selectAll('.y'+name)
   .style('display', 'block');
 }
 
@@ -386,28 +369,28 @@ for (let checkbox of document.querySelectorAll("input[type=checkbox]")) {
 }
 
 function startToolTip(year, country, artist_name, song_name){
-  d3.select("#tooltip").style('display', 'block');
-  d3.select("#country").html(country + ' ' + year );
-  d3.select('#artist').html(artist_name);
-  d3.select('#song').html(song_name); 
-  d3.selectAll('circle').transition().attr('r', '3').style('opacity', '.1');
-  d3.selectAll('.connect').transition().style('opacity', '.05');
-  d3.selectAll('.median').transition().style('opacity', '.05');
-  d3.selectAll('.y'+ year).transition().attr('r', '6').style('opacity', '1');
-    //  d3.select('#EV-flag-spez').attr('src', 'flags/1x1/' + country + '.SVG')
+  select("#tooltip").style('display', 'block');
+  select("#country").html(country + ' ' + year );
+  select('#artist').html(artist_name);
+  select('#song').html(song_name); 
+  selectAll('circle').transition().attr('r', '3').style('opacity', '.1');
+  selectAll('.connect').transition().style('opacity', '.05');
+  selectAll('.median').transition().style('opacity', '.05');
+  selectAll('.y'+ year).transition().attr('r', '6').style('opacity', '1');
+    //  select('#EV-flag-spez').attr('src', 'flags/1x1/' + country + '.SVG')
     
 }  
 function stopToolTip (year){
-  d3.select("#tooltip").style('display', 'none');
-  d3.selectAll('circle').transition().attr('r', '4').style('opacity','1');
-  d3.selectAll('.connect').transition().attr('r', '4').style('opacity','1');
-  d3.selectAll('.median').transition().style('opacity', '1');   
+  select("#tooltip").style('display', 'none');
+  selectAll('circle').transition().attr('r', '4').style('opacity','1');
+  selectAll('.connect').transition().attr('r', '4').style('opacity','1');
+  selectAll('.median').transition().style('opacity', '1');   
 } 
 
 stopToolTip(); 
-let tooltip = d3.select('#datavis')
+let tooltip = select('#datavis')
     .on('mousemove', function(event){
-        let coords = d3.pointer(event);
-        d3.select("#tooltip").style('top', 50+coords[1]+"px")
+        let coords = pointer(event);
+        select("#tooltip").style('top', 50+coords[1]+"px")
                              .style('left', 45+ coords[0]+"px");
             })
